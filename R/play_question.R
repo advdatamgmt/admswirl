9d85bc12a0a8b39f9b2e16309499d1ff87b2ba36
bbbruce <bbbruce@emory.edu>
2018-09-05 10:50:02 -0400

    Fixed bug that prevented moving forward on play_question if there was no valid R input before nxt()

--- R/play_question.R
+++ R/play_question.R
@@ -10,7 +10,7 @@ do_nxt.play_question <- function(e) {
   # resets environment to 'default' and does not
   # start the question over like the 'default' version
   if (length(e$snapshot) > 0)
-    xfer(as.environment(e$snapshot), globalenv())
+    swirl:::xfer(as.environment(e$snapshot), globalenv())
   swirl:::swirl_out(swirl:::`%N%`(swirl:::s(), "Resuming lesson..."))
   class(e) <- c("environment", "default")
   e$playing <- FALSE
